193280fa02ec423d45fc5fc7397de9bd4cf5c65e
vaibhav <vaibhavagarwal277@gmail.com>
2025-08-11 22:33:08 +0530

    made corrections during deployment

--- src/components/FormLayout.jsx
+++ src/components/FormLayout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function FormLayout({ children }) {
+function FormLayOut({ children }) {
   return (
     <div className="min-h-screen w-full bg-[#f8fafc] flex items-center justify-center relative">
       <div
@@ -22,4 +22,4 @@ function FormLayout({ children }) {
   );
 }
 
-export default FormLayout;
+export default FormLayOut;
